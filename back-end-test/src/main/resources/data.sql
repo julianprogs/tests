ecaa1d367e8da15a7a22741b4ced1b62ae23df05
julianprogs <49076468+julianprogs@users.noreply.github.com>
2019-04-02 14:20:11 -0300

    Add files via upload
    Recursos

---
+++ back-end-test/src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO TEST.CATEGORY VALUES
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Alimentos'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Eletrodomésticos'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Móveis'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Brinquedos'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Brinquedos Elétricos'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Medicamentos');
+
+INSERT INTO TEST.PRODUCT VALUES
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Arroz', 1),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Feijão', 1),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Aspirador de pó', 2),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Batedeira', 2),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Liquidificador', 2),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Sofá', 3),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Mesa', 3),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Estante', 3);
+       
+INSERT INTO TEST.TOY VALUES
+       (TEST.TOY_SEQ.NEXTVAL, 'Boneca', 4, ),
+       (TEST.TOY_SEQ.NEXTVAL, 'Carrinho', 4),
+       (TEST.TOY_SEQ.NEXTVAL, 'Carrinho Controle Remoto', 5),
+       (TEST.TOY_SEQ.NEXTVAL, 'Drone', 5),
+       (TEST.TOY_SEQ.NEXTVAL, 'Video Game', 5);
+       
+INSERT INTO TEST.MEDICINE VALUES
+       (TEST.MEDICINE_SEQ.NEXTVAL, 'Gen 1', 6),
+       (TEST.MEDICINE_SEQ.NEXTVAL, 'Gen 2', 6),
+       (TEST.MEDICINE_SEQ.NEXTVAL, 'Gen 3', 6); 
